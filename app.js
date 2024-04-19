5e3288fe3e0b87d78dfe2b24f114fb5415f21a62
Musma-n <154560114+Musma-n@users.noreply.github.com>
2024-04-19 21:17:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let myArr = [[2, 3, 5, 6, 7, 9], [1, 5], [4], [7, 3, 8]];
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 4; j++) {
+    console.log(`t : ${t}  a : ${a}`);
+    console.log(myArr[t][a]);
+  }
+}
